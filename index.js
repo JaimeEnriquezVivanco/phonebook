1e9b257c42826f4623c0374b34367b341d7a3c70
Jaime Enriquez <jaime.enriquez.vivanco@gmail.com>
2025-07-05 19:40:54 -0500

    ex 3.9

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-// app.use(morgan('tiny'))
-// tiyn
-// :method :url :status :res[content-length] - :response-time ms
+app.use(cors())
 
 morgan.token('body', (req) => {
     return JSON.stringify(req.body)
